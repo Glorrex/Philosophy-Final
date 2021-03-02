8c31f628079d046898169badb76c2504068b476b
Glorrex <zstoneburner@gmail.com>
2021-03-02 17:32:15 -0500

    Quit button broke, need to update the code.

--- Assets/Scripts/UI/StartGame.cs
+++ Assets/Scripts/UI/StartGame.cs
@@ -8,21 +8,21 @@ using UnityEngine.UI;
 public class StartGame : MonoBehaviour
 {
     public Button startButton;
-    public Button MainMenuButton;
+   // public Button MainMenuButton;
     void Start()
     {
         startButton.onClick.AddListener(RestartGame);
-        MainMenuButton.onClick.AddListener(MainMenu);
+        //MainMenuButton.onClick.AddListener(MainMenu);
     }
     public void RestartGame()
     {
         SceneManager.LoadScene("Main Scene");
         PlayerPrefs.DeleteAll();
     }
-    public void MainMenu()
-    {
-        SceneManager.LoadScene("Philosophy Final");
-        PlayerPrefs.DeleteAll();
+    //public void MainMenu()
+    //{
+      //  SceneManager.LoadScene("Philosophy Final");
+        //PlayerPrefs.DeleteAll();
     }
 
-}
+//}
