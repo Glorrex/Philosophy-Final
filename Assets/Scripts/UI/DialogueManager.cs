b308d8fae5297134330e0f3d7189a7666adbc9b7
Glorrex <zstoneburner@gmail.com>
2021-03-04 13:25:37 -0500

    Implemented basic dialogue!!

--- Assets/Scripts/UI/DialogueManager.cs
+++ Assets/Scripts/UI/DialogueManager.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
-
+    public Text nameText;
+    public Text dialogueText;
     private Queue<string> sentences;
     // Start is called before the first frame update
     void Start()
@@ -14,7 +16,8 @@ public class DialogueManager : MonoBehaviour
 
     public void StartDialogue (Dialogue dialogue)
     {
-        Debug.Log("starting conversation with" + dialogue.name);
+        //Debug.Log("starting conversation with" + dialogue.name);
+        nameText.text = dialogue.name;
         sentences.Clear();
         foreach (string sentence in dialogue.sentences)
         {
@@ -30,10 +33,10 @@ public class DialogueManager : MonoBehaviour
             return;
         }
         string sentence = sentences.Dequeue();
-        Debug.Log(sentence);
+        dialogueText.text = sentence;
     }
     void EndDialogue()
     {
-        Debug.Log("End of conversation.");
+        //Debug.Log("End of conversation.");
     }
 }
