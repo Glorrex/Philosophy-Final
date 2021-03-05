0e59b4817bad1d1e053f0504a4de22fe719cda5f
Glorrex <zstoneburner@gmail.com>
2021-03-05 11:18:01 -0500

    Changed the trigger for the dialogue to happen when the player touches an empy game object with the script attached to it.

--- Assets/Scripts/UI/DialogueTrigger.cs
+++ Assets/Scripts/UI/DialogueTrigger.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
-    public void TrigerDialogue()
+    //public void TrigerDialogue()
+    //{
+    //  FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    //}
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        if (collision.CompareTag("Player"))
+        {
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        }
     }
 }
